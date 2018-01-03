bc35b0635be616fc38a58955102b7a5a9a0805d4
marek.kapowicki <marek.kapowicki@gmail.com>
2018-01-03 23:23:24 +0100

    typo

--- aggregator/src/test/java/com/bol/test/assignment/aggregator/AggregatorServiceWithAssertJTest.java
+++ aggregator/src/test/java/com/bol/test/assignment/aggregator/AggregatorServiceWithAssertJTest.java
@@ -17,7 +17,7 @@ import static com.bol.test.assignment.offer.OfferCondition.AS_NEW;
 import static com.bol.test.assignment.offer.OfferCondition.UNKNOWN;
 
 
-public class AggregatorServiceWithAsserJTest implements WithAssertions, WithBDDMockito {
+public class AggregatorServiceWithAssertJTest implements WithAssertions, WithBDDMockito {
     private OrderService orderService = mock(OrderService.class);
 
     private OfferService offerService = mock(OfferService.class);

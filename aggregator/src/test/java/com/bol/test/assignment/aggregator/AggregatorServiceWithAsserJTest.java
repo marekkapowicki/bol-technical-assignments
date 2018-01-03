854e9857b011ee5b3f4d2b166e87cd504f0547db
marek.kapowicki <marek.kapowicki@gmail.com>
2018-01-03 23:00:44 +0100

    unit tests based on assertj

---
+++ aggregator/src/test/java/com/bol/test/assignment/aggregator/AggregatorServiceWithAsserJTest.java
@@ -0,0 +1,96 @@
+package com.bol.test.assignment.aggregator;
+
+import com.bol.test.assignment.offer.Offer;
+import com.bol.test.assignment.offer.OfferService;
+import com.bol.test.assignment.order.Order;
+import com.bol.test.assignment.order.OrderService;
+import com.bol.test.assignment.product.Product;
+import com.bol.test.assignment.product.ProductService;
+import info.solidsoft.mockito.java8.api.WithBDDMockito;
+import org.assertj.core.api.WithAssertions;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+
+import java.util.concurrent.ExecutionException;
+
+import static com.bol.test.assignment.offer.OfferCondition.AS_NEW;
+import static com.bol.test.assignment.offer.OfferCondition.UNKNOWN;
+
+
+public class AggregatorServiceWithAsserJTest implements WithAssertions, WithBDDMockito {
+    private OrderService orderService = mock(OrderService.class);
+
+    private OfferService offerService = mock(OfferService.class);
+
+    private ProductService productService = mock(ProductService.class);
+
+    private AggregatorService aggregatorService = new AggregatorService(orderService, offerService, productService);
+
+
+    private final int sellerId = 1;
+    private final int orderId = 2;
+    private final int offerId = 3;
+    private final int productId = 4;
+    private String title = "Title";
+
+
+    @Test
+    public void simpleHappyFlow() throws ExecutionException, InterruptedException {
+        //Given
+        given(orderService.getOrder(sellerId)).willReturn(new Order(orderId, offerId, productId));
+        given(offerService.getOffer(offerId)).willReturn(new Offer(offerId, AS_NEW));
+        given(productService.getProduct(productId)).willReturn(new Product(productId, title));
+
+        //When
+        EnrichedOrder enrichedOrder = aggregatorService.enrich(sellerId);
+
+        //Then
+        assertThat(enrichedOrder.getId()).isEqualTo(orderId);
+
+    }
+
+    @Test(timeout = 2000)
+    public void offerAndProductServicesAreSlow() throws InterruptedException, ExecutionException {
+        //Given
+        given(orderService.getOrder(sellerId)).willReturn(new Order(orderId, offerId, productId));
+        given(offerService.getOffer(offerId)).willAnswer(
+                (InvocationOnMock invocationOnMock) -> {
+                    Thread.sleep(1500);
+                    return new Offer(offerId, AS_NEW);
+                }
+        );
+        given(productService.getProduct(productId)).willAnswer(
+                (InvocationOnMock invocationOnMock) -> {
+                    Thread.sleep(1500);
+                    return new Product(productId, title);
+                }
+        );
+
+        //When
+        EnrichedOrder enrichedOrder = aggregatorService.enrich(sellerId);
+
+        //Then
+        EnrichedOrderAssert.assertThat(enrichedOrder)
+                .hasId(orderId)
+                .hasOfferCondition(AS_NEW)
+                .hasProductTitle(title);
+    }
+
+    @Test
+    public void offerServiceFailed() throws ExecutionException, InterruptedException {
+        //Given
+        given(orderService.getOrder(sellerId)).willReturn(new Order(orderId, offerId, productId));
+        given(offerService.getOffer(offerId)).willThrow(new RuntimeException("Offer Service failed"));
+        given(productService.getProduct(productId)).willReturn(new Product(productId, title));
+
+        //When
+        EnrichedOrder enrichedOrder = aggregatorService.enrich(sellerId);
+
+        //Then
+        EnrichedOrderAssert.assertThat(enrichedOrder)
+                .hasId(orderId)
+                .hasProductTitle(title)
+                .hasOfferId(-1)
+                .hasOfferCondition(UNKNOWN);
+    }
+}
---
+++ aggregator/src/test/java/com/bol/test/assignment/aggregator/EnrichedOrderAssert.java
@@ -0,0 +1,53 @@
+package com.bol.test.assignment.aggregator;
+
+import com.bol.test.assignment.offer.OfferCondition;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
+
+class EnrichedOrderAssert extends AbstractAssert<EnrichedOrderAssert, EnrichedOrder> {
+
+
+    private EnrichedOrderAssert(EnrichedOrder enrichedOrder) {
+        super(enrichedOrder, EnrichedOrderAssert.class);
+    }
+
+    static EnrichedOrderAssert assertThat(EnrichedOrder actual) {
+        return new EnrichedOrderAssert(actual);
+    }
+
+    public EnrichedOrderAssert hasId(int id) {
+        isNotNull();
+        Assertions.assertThat(actual.getId()).isEqualTo(id);
+        return this;
+    }
+
+    public EnrichedOrderAssert hasOfferId(int offerId) {
+        isNotNull();
+        Assertions.assertThat(actual.getOfferId()).isEqualTo(offerId);
+        return this;
+    }
+
+    public EnrichedOrderAssert hasOfferCondition(OfferCondition offerCondition) {
+        isNotNull();
+        Assertions.assertThat(actual.getOfferCondition()).isEqualTo(offerCondition);
+        return this;
+    }
+
+    public EnrichedOrderAssert hasProductId(int productId) {
+        isNotNull();
+        Assertions.assertThat(actual.getProductId()).isEqualTo(productId);
+        return this;
+    }
+
+    public EnrichedOrderAssert hasProductTitle(String productTitle) {
+        isNotNull();
+        Assertions.assertThat(actual.getProductTitle()).isEqualTo(productTitle);
+        return this;
+    }
+
+    public EnrichedOrderAssert hasNullProductTitle() {
+        isNull();
+        return this;
+    }
+
+}
